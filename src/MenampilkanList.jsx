491de4c7c9e4e3f21ef1956e7139c32695599e89
Marwan <marwahpinrang6@gmail.com>
2023-10-24 15:44:46 +0800

    memperbaiki nama variable

--- src/MenampilkanList.jsx
+++ src/MenampilkanList.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Navigation } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
 import React from "react";
-import { ShimmerPostItem } from "react-shimmer-effects";
-
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
@@ -13,52 +10,40 @@ import ShowModalShimmer from './components/Shimmers/ShowModalShimmer';
 import ModalMovie from './components/ResultModal/modalMovie';
 
 
-export default function AkanDihapus() {
 
+export default function MenampilkanList() {
     // state ini dijadikan tempat penampungan data film yang telah diambil menggunakan method fetch
     // dan nilai defaultnya harus array kosong agar method map tidak error
     const [dataMovie, setDataMovie] = useState([]);
-
-
     // fungsi state ini untuk mengaktifkan/nonaktifkan efek shimmer
     // defaultnya shimmer nonaktif/false
     // ada 4 nilai yang akan dimasukkan kedalam state ini yaitu : false, true, not found, not connection
     // 
     const [statusShimmer, setStatusShimmer] = useState(false)
-
     const API_KEY = 'aebedaef';
-
-    function getData() {
-
-        const NAME_MOVIE = document.getElementById('inputan');
-        const URL_BASE_TO_GET_BY_NAME = `http://www.omdbapi.com/?s=${NAME_MOVIE.value}&apikey=${API_KEY}`;
-
+    const BASE_URL = `http://www.omdbapi.com/`;
+    function GetData() {
+        let getNameMovie = document.getElementById('inputan');
+        let endPoinGetMovies = `${BASE_URL}?s=${getNameMovie.value}&apikey=${API_KEY}`;
         // ketika data film dalam proses pengambilan
         // aktifkan efek shimmer dengan kata keyword true
         setStatusShimmer(true)
-
-
-        fetch(URL_BASE_TO_GET_BY_NAME)
+        fetch(endPoinGetMovies)
             .then(response => {
-
                 // jika terdapat masalah pada url movie maka akan menampilan pesan dibawah
                 if (!response.ok) {
                     throw new Error('Ada yang salah pada link')
                 }
-
                 // jika proses lancar, promise akan dirubah menjadi data json
                 return response.json()
             })
             .then(datas => {
-
                 // jika nama film tidak ditemukan maka error akan ditangkap dan dilempar ke method catch
                 if (datas.Response === 'False') {
                     throw new Error(datas.Error)
                 };
-
                 // jika proses berjalan lancar, efek shimmer akan dihilangkan dengan keyword false
                 setStatusShimmer(false)
-
                 // kemudian memasukkan data film ke dalam variable data pada state diatas
                 return (setDataMovie(datas.Search))
             })
@@ -68,7 +53,6 @@ export default function AkanDihapus() {
                     setStatusShimmer('not found')
                 }
                 // jika nama error adalah TypeError maka rubah nilai statusShimmer menjadi not connection
-
                 if (error.name === 'TypeError') {
                     setStatusShimmer('not connection')
                 }
@@ -78,46 +62,39 @@ export default function AkanDihapus() {
         <SwiperSlide key={movie.imdbID}>
             <img src={movie.Poster} alt={movie.Title} className='w-full' />
             <h3 className='text-white font-semibold text-xl my-2'>{movie.Title}</h3>
-            <button onClick={() => showModalDet(movie.imdbID)} >Detile</button>
+            <button onClick={() => ShowModalDetail(movie.imdbID)} >Detile</button>
         </SwiperSlide>
     )
-
-
     // ini variable untuk menampung detail movie, nilai defaut null/kosong
     const [detailMovie, setDetailMovie] = useState(null)
     const [StatusShowModal, setStatusShowModal] = useState(false);
-
-
-    function showModalDet(id) {
+    function ShowModalDetail(id) {
         setDetailMovie(null)
         const ID_MOVIE = id;
-        const URL_BASE_TO_GET_BY_ID = `http://www.omdbapi.com/?i=${ID_MOVIE}&apikey=${API_KEY}`
-        fetch(URL_BASE_TO_GET_BY_ID)
+        let endPointGetMovieDetail = `${BASE_URL}?i=${ID_MOVIE}&apikey=${API_KEY}`
+        fetch(endPointGetMovieDetail)
             .then(Response => Response.json())
             .then(datas => setDetailMovie(datas))
             .catch(err => console.log(err.name))
         setStatusShowModal(true)
     }
-
-    function closeModal() {
+    function CloseModal() {
         setStatusShowModal(false)
     }
-
-
-    function notFoundMovies() {
+    function NotFoundMovies() {
         return (
             <h1 className='text-center mt-8'>Movies Not Found</h1>
         )
     }
-    function notConnectionNetwork() {
+    function NotConnectionNetwork() {
         return (
             <h1>Not Connection</h1>
         )
     }
-    function showModal(movie) {
+    function ShowModal(movie) {
         if (StatusShowModal) {
             if (detailMovie) {
-                return <ModalMovie movie={movie} btnClose={closeModal} />
+                return <ModalMovie movie={movie} btnClose={CloseModal} />
             }
             if (!detailMovie) {
                 return <ShowModalShimmer />
@@ -127,27 +104,23 @@ export default function AkanDihapus() {
             return ''
         }
     }
-
-    function searchResult() {
+    function SearchResult() {
         if (statusShimmer === true) {
             return <ShowShimmer />
         }
         if (statusShimmer === 'not found') {
-            return notFoundMovies()
+            return NotFoundMovies()
         }
         if (statusShimmer === 'not connection') {
-            return notConnectionNetwork()
+            return NotConnectionNetwork()
         }
-
         return listFilm;
     }
-
-
     return (
         <div className='absolute w-[90vw] -translate-x-1/2 left-1/2 -translate-y-1/2 xl:top-1/2 mt-8 mx-auto'>
             <div className='w-fit mx-auto flex gap-1 items-center'>
                 <input type="text" id='inputan' className=' rounded-lg text-xl p-[.35rem] bg-white text-black border-2 border-black' />
-                <button className='rounded-1 border' onClick={getData}>Get Data</button>
+                <button className='rounded-1 border' onClick={GetData}>Get Data</button>
             </div>
             <Swiper
                 slidesPerView={1}
@@ -169,14 +142,9 @@ export default function AkanDihapus() {
                 }}
                 modules={[Navigation]}
                 className="w-full  right-0 bottom-0 my-8">
-                {searchResult()}
-
+                {SearchResult()}
             </Swiper>
-            {showModal(detailMovie)}
+            {ShowModal(detailMovie)}
         </div>
     )
 }
-
-
-// Naming Conventions for react js
-// https://dev.to/sathishskdev/part-1-naming-conventions-the-foundation-of-clean-code-51ng
